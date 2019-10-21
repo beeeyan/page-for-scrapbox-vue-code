18445ffd8c285d259928041bd528d0d9cf5ae3d4
sakasanokuma <moonsunphoenix@gmail.com>
2019-10-22 02:39:44 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function loadScript(url) {
+    return new Promise(
+        (resolve) => {
+            const script = document.createElement('script');
+            script.src = url;
+            script.onload = resolve;
+            document.body.appendChild(script);
+        })
+}
+
+async function loadAllScripts(urls) {
+    for(let url of urls){
+        await loadScript(url);
+    }
+}
+
+
+$(function () {
+    const args = {};
+    alert(document.referrer);
+    // ?以降の結果を取得する
+    document.location.search.substring(1).split('&').forEach(
+        (s) => {
+            let [name, value] = s.split('=');
+            // decode
+            args[name] = decodeURIComponent(value);
+        })
+    const codelist = args['code'];
+    console.log('codelist', codelist);
+
+    if (codelist) {
+        let urls = codelist.split(/,/);
+        loadAllScripts(urls);
+    }
+}
+
+)
